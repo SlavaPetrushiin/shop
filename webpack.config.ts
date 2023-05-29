711c77234c8cf6afdf034e387d10dc4909fb81ba
Slava <slava91petrushin@yandex.ru>
2023-05-29 22:51:32 +0300

    start settings webpack

---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import  path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+	mode: "development",
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+	devtool: 'inline-source-map',
+  
+	module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+	plugins: [
+		new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+
+
+  ],
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+		clean: true
+  }, 
+};
+
+export default config;
