283fbb53a3dbabafd49e828754814f01fc03f21b
Petrushin <petrushin@antivor.ru>
2023-07-03 16:36:29 +0300

    create sidebar

--- src/pages/RootPage/ui/Root.tsx
+++ src/pages/RootPage/ui/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 export default function Root() {
     return (
         <>
             <Navbar />
-            <main>
+            <main className="main-page">
+                <Sidebar />
                 <Outlet />
             </main>
         </>
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { classNames } from "helpers/classNames/classNames";
+import React, { useState } from "react";
+import { Button } from "shared/Button";
+import cls from "./Sidebar.module.scss";
+
+interface SidebarProps {
+    className?: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = (props) => {
+    const { className = "" } = props;
+
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => setCollapsed((prev) => !prev);
+
+    return (
+        <div className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
+            <Button onClick={onToggle}>S</Button>
+        </div>
+    );
+};
+
+export default Sidebar;
